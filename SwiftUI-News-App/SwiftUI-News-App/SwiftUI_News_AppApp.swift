632ed09d64cf761180842ca3b95959dd20f1e1be
Sendeky <spruslasha@gmail.com>
2023-01-10 09:08:01 -0800

    initial commit

---
+++ SwiftUI-News-App/SwiftUI-News-App/SwiftUI_News_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_News_AppApp.swift
+//  SwiftUI-News-App
+//
+//  Created by RuslanS on 1/10/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_News_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
